25d50258802eb4c218513dc797e9dcd886fac377
winstoncwang <congwang@live.co.uk>
2022-06-29 11:07:34 +0100

    console log response

--- JS/SignIn.js
+++ JS/SignIn.js
@@ -9,15 +9,7 @@ function loginSubmit(e) {
     const password = document.getElementById("password").value;
     const url ="https://rocky-forest-99036.herokuapp.com/login"
     let response = postData(url, { "username": username, "password": password }).then(data => { console.log(data) });
-    if(response.status === 403){
-        Swal.fire({
-          icon: 'error',
-          title: 'too bad!',
-          text: `Username ${username} is allready in use!`
-        })
-       } else {
-         window.location.href = "../../Views/Feed/Feed.html"
-       }
+    console.log(response);
 }
  
 async function postData(url = '', data = {}) { 

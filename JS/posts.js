22a4ebd45e06fcc67902ec32f486fdeea8e47c8d
Student <Student@MA403.local>
2022-06-30 11:26:57 +0100

    breaking things

--- JS/posts.js
+++ JS/posts.js
@@ -59,9 +59,10 @@ async function getPosts() {
         postElement.setAttribute("class", "posts");
         //postElement.setAttribute("id", post['_links']['post']['href'].slice(-1));
         postElement.innerText = post.content
-        document.body.append(postElement);
-        document.body.append(editButton);
-        document.body.append(deleteButton);
+        let y = document.getElementById('postBox')
+        y.append(postElement);
+        y.append(editButton);
+        y.append(deleteButton);
         
         
       }
